5f3f078db6e97016c8cbeb973997880d2ea5389f
isLinHehea <105376348+isLinHehea@users.noreply.github.com>
2023-05-22 12:16:28 +0800

    LJJ

--- src/main/java/cse/java2/project/DataAnalyze.java
+++ src/main/java/cse/java2/project/DataAnalyze.java
@@ -1,5 +1,49 @@
 package cse.java2.project;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataAnalyze {
 
+    public static Connection con = null;
+    public static String user = "postgres";
+    public static String pwd = "123456";
+
+    private static void getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+
+        } catch (Exception e) {
+            System.err.println("Cannot find the PostgreSQL driver. Check CLASSPATH.");
+            System.exit(1);
+        }
+
+        try {
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            con = DriverManager.getConnection(url, user, pwd);
+
+        } catch (SQLException e) {
+            System.err.println("Database connection failed");
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    private static void closeConnection() {
+        if (con != null) {
+            try {
+                con.close();
+                con = null;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        getConnection();
+
+        closeConnection();
+    }
 }
--- src/main/java/cse/java2/project/DataFetch.java
+++ src/main/java/cse/java2/project/DataFetch.java
@@ -253,6 +253,4 @@ public class DataFetch {
             e.printStackTrace();
         }
     }
-
-
 }
